8d17fc54a930c2f4e9b404674ddf3eb742730f3e
Esther Anne Robb <erobb@sgiuv300-srcf-d10-01.scg.stanford.edu>
2023-06-20 08:48:21 -0700

    update run_all script

---
+++ run_all.sh
@@ -0,0 +1,12 @@
+
+
+pop=$1
+
+python scripts/eqtls.sh $1
+python preprocess/residuals.R $1
+python preprocess/make_dataset_mp.py --pop $1
+python preprocess/add_gencode.py --pop $1
+python preprocess/add_phylop.py --pop $1
+python preprocess/aggregate_genes.py --pop $1
+python preprocess/add_pair_labels.py --pop $1 
+
