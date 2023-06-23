5f8393a823b5dc30f4738faa739f45cb3a98a7ab
Esther Anne Robb <erobb@smsh11dsu-srcf-d15-37.scg.stanford.edu>
2023-06-23 10:56:36 -0700

    fixes to gencode TES/TSS distance calculation

--- scripts/process_gencode.sh
+++ scripts/process_gencode.sh
@@ -5,7 +5,7 @@ data_dir="${oak}/data/watershed"
 gencodeprefix="gencode.v43.chr_patch_hapl_scaff.annotation"
 gencode="gencode.v43.chr_patch_hapl_scaff.annotation.gtf.gz"
 
-# for gencode annotation get lincRNA and prootein-coding exons, but also pad the internal exons by 5 base pairs
+# for gencode annotation get lincRNA and protein-coding exons, but also pad the internal exons by 5 base pairs
 python pad.gtf.exons.py $gencode | sort -k1,1 -k2,2n | uniq > ${gencodeprefix}_coding.lincRNA_padded.bed
 
 
