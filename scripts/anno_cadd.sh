cda2fa7f6a719069a0a4697742194e2e63cfb314
Esther Anne Robb <erobb@smsh11dsu-srcf-d15-37.scg.stanford.edu>
2023-08-23 14:45:07 -0700

    fix merge function for anno_cadd

--- scripts/anno_cadd.sh
+++ scripts/anno_cadd.sh
@@ -8,19 +8,28 @@ vcf=$4
 
 # Tell bcftools what columns we want from CADD file, based on input.cols (assumes 1 column per line)
 tabix -h $cadd 1:1-1 | tail -n 1 | sed 's/#//' | sed 's/\s/,/g' > all_columns.tmp
-cols=$(awk 'NR==FNR{a[$0];next}{for(i=1; i<=NF; i++) {if($i in a){printf $i","} else if($i == "Chrom" || $i == "Pos" || $i == "Ref" || $i == "Alt"){printf $i","}else{printf "-,"}}}' $cadd_cols FS="," all_columns.tmp)
+# Mark CADD columns to keep and discard
+cols=$(
+awk 'NR==FNR{a[$1];next}
+{for(i=1; i<=NF; i++) {
+    if($i in a || ($i == "Chrom" || $i == "Pos" || $i == "Ref" || $i == "Alt")){
+        printf $i","
+    }
+    else{printf "-,"}
+}
+}' FS=',' $cadd_cols FS="," all_columns.tmp)
 
 # Create VCF header info lines for relevant CADD annotations
 merge_logic=""
-while read anno; do
+while IFS=',' read -r anno merge; do
     echo "##INFO=<ID=${anno},Number=.,Type=String,Description=\"CADD annotation\">" >> header.tmp
-    merge_logic="${merge_logic}${anno}:max,"
+    merge_logic="${merge_logic}${anno}:${merge},"
 done < $cadd_cols
 
 for l in {1..22} X Y; do echo chr$l $l; done > rename_chr.tmp
 bcftools annotate --rename-chrs rename_chr.tmp $vcf \
 | bcftools annotate -a $cadd -c ${cols} -h header.tmp --merge-logic ${merge_logic} --min-overlap 1 \
-| bcftools annotate -a $cadd_indel -c ${cols} -h header.tmp --merge-logic ${merge_logic} --min-overlap 1 
+| bcftools annotate -a $cadd_indel -c ${cols} -h header.tmp --merge-logic ${merge_logic} --min-overlap 1
 
 rm rename_chr.tmp header.tmp all_columns.tmp
 
