d12172e5ae4a117303acb7924889587fc382b3a4
Esther Anne Robb <erobb@smsh11dsu-srcf-d15-37.scg.stanford.edu>
2023-08-28 14:20:48 -0700

    add helpful warning messages for missing columns in impute

--- scripts/impute_missing.R
+++ scripts/impute_missing.R
@@ -3,16 +3,24 @@
 args = commandArgs(trailingOnly=TRUE)
 tsv=args[1]
 impute=read.csv(args[2],header=FALSE, row.names=1)
+nout=as.numeric(args[3])
 
 df=read.table(tsv, fill=TRUE, header=1, sep="\t")
 
 # Replace missing values with imputation values
-for (col in row.names(impute)[1:2]) {
-  if (!(col %in% colnames(df))) {
-        warning(paste(col,"not found."))
-        next
-  }
+impute_cols=intersect(colnames(df), row.names(impute))
+
+missing=setdiff(row.names(impute),impute_cols)
+if (length(missing)) {
+warning(paste(c("Columns from config/impute not found in data:", missing), collapse=" "))
+}
+missing=setdiff(colnames(df)[3:(length(colnames(df))-2-nout)],impute_cols)
+if (length(missing)) {
+warning(paste(c("Columns from data not found in config/impute:", missing), collapse=" "))
+}
+
+for (col in impute_cols){
   df[is.na(df[,col]),col] = impute[col,]
 }
 
-write.table(df,"",quote=FALSE,row.names=FALSE, na="", sep="\t")
+write.table(df,"",quote=FALSE,row.names=FALSE, na="NA", sep="\t")
